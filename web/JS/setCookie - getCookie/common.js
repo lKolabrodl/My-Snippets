00a3ab5ac5d94017661343372d54a2a88f728839
Bril Mikhail <Kolabrod@mail.ru>
2020-09-01 17:14:08 +0300

    set get cook

---
+++ web/JS/setCookie - getCookie/common.js
@@ -0,0 +1,25 @@
+function setCookie(name, value) {
+  document.cookie = name + "=" + value;
+}
+
+function getCookie(name) {
+  let cookie = " " + document.cookie;
+  let search = " " + name + "=";
+  let setStr = null;
+  let offset = 0;
+  let end = 0;
+  if (cookie.length > 0) {
+    offset = cookie.indexOf(search);
+
+    if (offset != -1) {
+      offset += search.length;
+      end = cookie.indexOf(";", offset);
+
+      if (end == -1) {
+        end = cookie.length;
+      }
+      setStr = unescape(cookie.substring(offset, end));
+    }
+  }
+  return setStr;
+}
